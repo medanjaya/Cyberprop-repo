bc92191a70db185625af8ac71a18de76805bbb17
vincentlee2004 <165283616+vincentlee2004@users.noreply.github.com>
2024-10-19 00:41:21 +0700

    Create home_page.dart

---
+++ lib/agen/home_page.dart
@@ -0,0 +1,16 @@
+// home_page.dart
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Ini adalah Beranda'),
+    );
+  }
+}
+
+
+
