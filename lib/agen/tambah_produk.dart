ddcd4863f4ce1cac96785fc37e5899ef5df8849c
vincentlee2004 <165283616+vincentlee2004@users.noreply.github.com>
2024-10-26 15:29:11 +0700

    Sudah hapus video player

--- lib/agen/tambah_produk.dart
+++ lib/agen/tambah_produk.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart'; // Import for video playback
 
 class Tambah extends StatefulWidget {
   const Tambah({super.key});
@@ -13,40 +12,14 @@ class Tambah extends StatefulWidget {
 class _TambahState extends State<Tambah> {
   String dropdownValue = 'Rumah/ruko'; // Initial selection for dropdown menu
   XFile? _image; // Declaration for image file
-  VideoPlayerController? _videoController; // Declaration for video controller
   int _selectedIndex = 0; // Add selected index for BottomNavigationBar
 
   Future<void> _pickImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
     setState(() {
       _image = pickedFile;
-      _videoController?.dispose();
-      _videoController = null;
     });
-  }
-
-  Future<void> _pickVideo() async {
-    final picker = ImagePicker();
-    final pickedVideo = await picker.pickVideo(source: ImageSource.gallery);
-
-    if (pickedVideo != null) {
-      setState(() {
-        _image = null;
-        _videoController = VideoPlayerController.file(File(pickedVideo.path))
-          ..initialize().then((_) {
-            setState(() {}); // Update UI after initialization
-            _videoController!.play();
-          });
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _videoController?.dispose();
-    super.dispose();
   }
 
   void _onItemTapped(int index) {
@@ -65,7 +38,7 @@ class _TambahState extends State<Tambah> {
         ),
         backgroundColor: const Color.fromARGB(255, 167, 86, 86),
       ),
-      body: SingleChildScrollView( // Added SingleChildScrollView here
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -88,7 +61,6 @@ class _TambahState extends State<Tambah> {
                 ),
               ),
               const SizedBox(height: 16),
-              // Image picker section with SizedBox
               GestureDetector(
                 onTap: _pickImage,
                 child: Container(
@@ -101,8 +73,8 @@ class _TambahState extends State<Tambah> {
                   ),
                   child: _image != null
                       ? SizedBox(
-                          width: 150, // Set your desired width
-                          height: 150, // Set your desired height
+                          width: 150,
+                          height: 150,
                           child: Image.file(
                             File(_image!.path),
                             fit: BoxFit.cover,
@@ -112,34 +84,6 @@ class _TambahState extends State<Tambah> {
                 ),
               ),
               const SizedBox(height: 16),
-              // Video picker section with SizedBox
-              GestureDetector(
-                onTap: _pickVideo,
-                child: Container(
-                  width: 500,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(8),
-                    color: _videoController != null
-                        ? Colors.transparent
-                        : Colors.grey[200],
-                  ),
-                  child: _videoController != null &&
-                          _videoController!.value.isInitialized
-                      ? SizedBox(
-                          width: 150, // Set your desired width
-                          height: 150, // Set your desired height
-                          child: AspectRatio(
-                            aspectRatio: _videoController!.value.aspectRatio,
-                            child: VideoPlayer(_videoController!),
-                          ),
-                        )
-                      : const Icon(Icons.videocam),
-                ),
-              ),
-              const SizedBox(height: 16),
-              // Dropdown menu for property type
               DropdownButton<String>(
                 value: dropdownValue,
                 icon: const Icon(Icons.arrow_drop_down),
@@ -167,7 +111,6 @@ class _TambahState extends State<Tambah> {
                 }).toList(),
               ),
               const SizedBox(height: 16),
-              // Alamat Field
               const Text(
                 'Alamat',
                 style: TextStyle(
@@ -185,47 +128,43 @@ class _TambahState extends State<Tambah> {
                 ),
               ),
               const SizedBox(height: 16),
-              // Tambah di antara 'Alamat' dan 'Deskripsi'
-const Text(
-  'Ukuran Properti',
-  style: TextStyle(
-    color: Color.fromARGB(255, 167, 86, 86),
-    fontWeight: FontWeight.bold,
-    fontSize: 16,
-  ),
-),
-const SizedBox(height: 8),
-Row(
-  children: [
-    // TextField untuk panjang
-    Expanded(
-      child: TextField(
-        decoration: const InputDecoration(
-          labelText: 'Panjang (meter)',
-          filled: true,
-          fillColor: Color.fromARGB(255, 233, 239, 214),
-          border: InputBorder.none,
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    ),
-    const SizedBox(width: 16), // Memberi jarak di antara kedua TextField
-    // TextField untuk lebar
-    Expanded(
-      child: TextField(
-        decoration: const InputDecoration(
-          labelText: 'Lebar (meter)',
-          filled: true,
-          fillColor: Color.fromARGB(255, 233, 239, 214),
-          border: InputBorder.none,
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    ),
-  ],
-),
-const SizedBox(height: 16),
-              // Deskripsi Field
+              const Text(
+                'Ukuran Properti',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 167, 86, 86),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Panjang (meter)',
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 233, 239, 214),
+                        border: InputBorder.none,
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Lebar (meter)',
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 233, 239, 214),
+                        border: InputBorder.none,
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
               const Text(
                 'Deskripsi',
                 style: TextStyle(
@@ -242,48 +181,43 @@ const SizedBox(height: 16),
                   border: InputBorder.none,
                 ),
               ),
-
-              // Harga Field
-const Text(
-  'Harga',
-  style: TextStyle(
-    color: Color.fromARGB(255, 167, 86, 86),
-    fontWeight: FontWeight.bold,
-    fontSize: 16,
-  ),
-),
-const SizedBox(height: 8),
-TextField(
-  decoration: const InputDecoration(
-    prefixText: 'Rp ',
-    filled: true,
-    fillColor: Color.fromARGB(255, 233, 239, 214),
-    border: InputBorder.none,
-  ),
-  keyboardType: TextInputType.number, // To ensure numeric input
-),
-const SizedBox(height: 16),
-
-// Submit Button
-Center(
-  child: ElevatedButton(
-    onPressed: () {
-      Navigator.pushReplacementNamed(context, '/menu'); // Navigates back to 'menu.dart'
-    },
-    style: ElevatedButton.styleFrom(
-      backgroundColor: const Color.fromARGB(255, 167, 86, 86),
-      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-    ),
-    child: const Text(
-      'Submit',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 16,
-      ),
-    ),
-  ),
-),
-            
+              const Text(
+                'Harga',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 167, 86, 86),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                decoration: const InputDecoration(
+                  prefixText: 'Rp ',
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 233, 239, 214),
+                  border: InputBorder.none,
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 16),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/menu');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 167, 86, 86),
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                  ),
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -305,8 +239,7 @@ Center(
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        backgroundColor:
-            const Color.fromARGB(255, 255, 223, 183), // Warna latar belakang
+        backgroundColor: const Color.fromARGB(255, 255, 223, 183),
       ),
     );
   }
